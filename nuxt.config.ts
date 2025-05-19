62c7a8241b856d41937a530b6c96faedc3522158
Loic Bellinger <loic.bellinger@gmail.com>
2025-05-19 11:11:20 +0200

    build: add icon.clientBundle & provider options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,5 +25,18 @@ export default defineNuxtConfig({
         commaDangle: 'never'
       }
     }
+  },
+  icon: {
+    // Don’t fetch from any API at runtime
+    provider: 'none',
+
+    // Bundle every icon referenced in the source
+    clientBundle: {
+      // scan all the components for `<Icon name="..." />`
+      scan: true,
+
+      // Fixed list:
+      icons: ['lucide:refresh-cw', 'lucide:chevron-down', 'lucide:mail', 'lucide:phone', 'lucide:github', 'lucide:linkedin']
+    }
   }
 })
